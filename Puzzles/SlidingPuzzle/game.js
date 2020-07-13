34b7412f8028a079175632a826e427c2a568f769
Paul Manley <pjmanley671@gmail.com>
2020-07-13 18:40:58 -0500

    Architecture overhaul
    Made massive changes to code structure .

--- Puzzles/SlidingPuzzle/game.js
+++ Puzzles/SlidingPuzzle/game.js
@@ -60,33 +60,177 @@ Any value returned is ignored.
  *		Fix hover issue for resolving completion.
  *		Add in cover image for detailing puzzle layout and interactions.
  *		
- * Version: 2.0
+ * Version: 2.1
  * Perlenspiel Package: 3.3
 */
 
 var G = function ()
 {
-	var mapData;
+	var mapData = [];
+	var colorOptions = [PS.COLOR_ORANGE, PS.COLOR_GREEN, PS.COLOR_INDIGO];
+	var shuffle = false;
+	var emptyTile = 
+	{
+		x:3,
+		y:3,
+		color:PS.COLOR_WHITE
+	};
+	
+	var SetupGrid = function()
+	{
+		PS.gridSize(9, 4);
+		PS.color(4, PS.ALL, PS.COLOR_BLACK);
+		PS.color(4, 0, PS.COLOR_GRAY);
+		PS.glyph(4, 0, "R");
+		PS.border(PS.ALL, PS.ALL, 0);
+	};
+	
+	var FillMapData = function()
+	{
+		var x, y, pos;
+		
+		for(y = 0; y < 4; y++)
+		{
+			for(x = 0; x < 4; x++)
+			{
+				pos = (y * 4) + x;
+				mapData[pos] = colorOptions[PS.random(3) - 1];
+			}
+		}
+		
+		mapData[pos] = emptyTile.color;
+	};
+	
+	var DrawMap = function()
+	{
+		var x, y, data;
+		
+		for(y = 0; y < 4; y++)
+		{
+			for(x = 0; x < 4; x++)
+			{
+				data = mapData[( y * 4 ) + x];
+				PS.color( x, y, data );
+				PS.color( (x + 5), y, data );
+			}
+		}
+	};
+	
+	var ValidateSwap = function(nx, ny)
+	{
+		if(nx > -1 && ny > -1)
+			if(nx < 4 && ny < 4)
+				if(Math.abs(nx - emptyTile.x) < 2 && Math.abs(ny - emptyTile.y) < 2)
+					return true;
+				
+		return false;
+	};
+	
+	var CheckWin = function()
+	{
+		var win = true;
+		
+		for(var x = 0; x < 4; x++)
+		{
+			for(var y = 0; y < 4; y++)
+			{
+				if(mapData[(y * 4) + x] !== PS.color(x, y))
+					win = false;
+			}
+		}
+		
+		return win;
+	};
+	
+	var SwapTiles = function(nx, ny)
+	{
+		if(ValidateSwap(nx, ny))
+		{
+			PS.color(emptyTile.x, emptyTile.y, PS.color(nx, ny));
+			PS.color(nx, ny, emptyTile.color);
+			emptyTile.x = nx;
+			emptyTile.y = ny;
+			
+			if(!shuffle)
+			{
+				PS.audioPlay("fx_click");				
+				if(emptyTile.x === 3 && emptyTile.y === 3)
+				{
+					if(CheckWin()) PS.statusText("Win!");
+				}
+			}
+		}
+	};
+	
+	var ShuffleBoard = function()
+	{
+		shuffle = true;
+		var minNumMoves = Math.pow(4, 4);
+		var i = 0, x = 0, y = 0;
+		do
+		{
+			x = (x === 0) ? PS.random(3) - 2 : 0;
+			y = (x != 0) ? PS.random(3) - 2 : 0;
+			
+			SwapTiles(emptyTile.x + x, emptyTile.y + y);
+			
+			if(i < minNumMoves)i++;
+			
+			if(emptyTile.x + emptyTile.y < 6 )i--;
+		}while(i < minNumMoves);
+		
+		shuffle = false;
+	};
 	
 	var exports =
 	{
-		init: function()
-		{
-			PS.gridSize(9, 4);
-			PS.color(4, PS.ALL, PS.COLOR_BLACK);
-			PS.color(4, 0, PS.COLOR_GRAY);
-			PS.glyph(4, 0, "R");
-			PS.border(PS.ALL, PS.ALL, 0);
+		Init: function()
+		{	
+			SetupGrid();
+			emptyTile.x = 3;
+			emptyTile.y = 3;
+			FillMapData();
+			DrawMap();
+			ShuffleBoard();
 		},
 		
 		OnPress: function(key, shift, ctrl, options)
 		{
+			var x, y;
+			switch (key) 
+			{
+				case PS.KEY_ARROW_UP:
+				case 119:
+					y -= 1;
+					break;
+				case PS.KEY_ARROW_DOWN:
+				case 115:
+					y += 1;
+					break;
+				case PS.KEY_ARROW_LEFT:
+				case 97:
+					x -= 1;
+					break;
+				case PS.KEY_ARROW_RIGHT:
+				case 100:
+					x += 1;
+					break;
+				case 114:
+					exports.Init();
+					break;
+				default:
+					break;
+			}
 			
+			SwapTiles(emptyTile.x + x, emptyTile.y + y);
 		},
 		
 		OnTouch: function(x, y, data, options)
 		{
+			SwapTiles(x, y);
 			
+			if(x === 4 && y === 0)
+				exports.Init();
 		}
 		
 	};
@@ -94,7 +238,7 @@ var G = function ()
 }();
 
 
-PS.init = G.init;
+PS.init = G.Init;
 PS.keyDown = G.OnPress;
 PS.touch = G.OnTouch;
 
