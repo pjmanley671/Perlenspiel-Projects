4fd9bfaf444008f6b3a49f4ce82a8e7d5b588579
Paul Manley <pjmanley671@gmail.com>
2020-07-18 12:56:44 -0500

    Refactoring Space Invaders
    Beginning refactoring of space invaders to better define code architecture.

--- Games/Space Invaders/game.js
+++ Games/Space Invaders/game.js
@@ -66,410 +66,92 @@ Any value returned is ignored.
 */
 var G = function()
 {
-	var winState = false; // initializes and sets the win state.
-	var w = 32, h = 32; // initializes and sets the width and height of the play space.
-	var sTimer, uTimer, dTimer, gTimer; // initializes the timer variables.
-	var shotReady = false, ufoReady = false; // initializes and sets the player unique abilities.
-	var shots = [], ufos = []; // initializes the arrays for the shots and ufos.
-
-	var makeShot = function() // create a new shot object.
+	var urlSource = "https://pjmanley671.github.io/game-files/SpaceInvaders/images/";
+	var width = 32, height = 32;
+	var shots = [null], ufos = [null];
+	var gameStates = {win: false, shot: false, ufo: false};
+	var timerIntervals = {update : 1, shot : 2, ufo : 120, defender : 4, step : 0};
+	var myTimer, drawTimer;
+	
+	var OnTick = function()
 	{
-		if(shotReady === false) // checks to make sure that the shot ability is ready.
-			return;
-
-		var myShot = // creates the new shot.
-			{
-				x : 0, y : 0,  // initializes the x and y coordinates and sets the color.cColor : PS.COLOR_GREEN
-				setSpawn : function(sx, sy) // sets the shots spawn location.
-				{
-                    this.x = sx;
-                    this.y = sy - 2; // offsets the y location to be 1 higher.
-                },
-
-				move : function(cx, cy) // moves the shot.
-				{
-					this.x = this.x + cx;
-					this.y = this.y + cy;
-				}
-			};
-		myShot.setSpawn(defender.x, defender.y); // sets the new shot at according to the  defenders position.
-		shots.unshift(myShot); // adds the new shot to the list of shots.
-		shotReady = false; // disables the defenders ability to shoot.
-		PS.audioPlay("fx_bang");
-	};
-
-	var makeUfo = function() // create a new ufo.
-	{
-		if(ufoReady === false) // checks to see if the ufo ability is ready.
-			return;
-		if(exports.inSpawn()) // checks to see if the spawn location is clear.
-			return;
-
-		var myUfo = // initializes the new ufo.
-			{
-				x : 0, y : 0, //cColor : PS.COLOR_MAGENTA
-				setSpawn : function() // sets the ufos spawn.
-				{
-					this.x = Math.floor(w / 2); // sets the ufos spawn to be half the width of the board.
-					this.y = 2;
-				},
-
-				move : function(cx, cy) // moves the ufo.
-				{
-					if(exports.inBounds(this.x + cx, this.y + cy)) // makes sure that the location being moved to is in the game space.
-					{
-						if(exports.ufoCollision(this.x + cx, this.y + cy)) // makes sure that this ufo doesn't collide with other ufos.
-							return;
-
-                        this.x = this.x + cx;
-                        this.y = this.y + cy;
-
-                        if(this.y >= h-4) // when moved the ufo checks to see if it has reached its destination.
-                        	winState = true;
-                    }
-				}
-			};
-		myUfo.setSpawn(); // tells the new ufo to set its spawn.
-		ufos.unshift(myUfo); // assigns the new ufo into the list.
-		ufoReady = false; // disables the ufos ability to produce more ufos.
-	};
-
-	var defender = // initializes the defender.
+		timerIntervals.step++;
+		
+		if(!gameStates.shot)
 		{
-			x : 0, y : 0,  // initializes the x and y coordinates and sets the color., cColor : PS.COLOR_RED
-			setSpawn : function() // sets the spawn of the defender
+			timerIntervals.defender++;
+			if(timerIntervals.defender == 4)
 			{
-				this.x = Math.floor(w / 2); // sets the x coordinate to half the width.
-				this.y = (h - 3); // sets the y coordinate to 2 higher than the height.
-			},
-
-			move : function(cx, cy) // move the defender.
-			{
-				if(exports.inBounds(this.x + cx, this.y + cy))
-                {
-                    this.x = this.x + cx;
-                    this.y = this.y + cy;
-                }
-			}
-		};
-
-	var myLoader = function(image)
-	{
-		var i;
-		if(image.source === "https://pjmanley671.github.io/game-files/SpaceInvaders/images/BackGround.bmp")
-			PS.imageBlit(image, 0, 0);
-
-		if(image.source === "https://pjmanley671.github.io/game-files/SpaceInvaders/images/defenderBase.bmp")
-			PS.imageBlit(image, defender.x - 2, defender.y+1);
-		if(image.source === "https://pjmanley671.github.io/game-files/SpaceInvaders/images/defenderBotMid.bmp")
-			PS.imageBlit(image, defender.x - 2, defender.y);
-		if(image.source === "https://pjmanley671.github.io/game-files/SpaceInvaders/images/defenderTopMid.bmp")
-			PS.imageBlit(image, defender.x - 1, defender.y - 1);
-		if(image.source === "https://pjmanley671.github.io/game-files/SpaceInvaders/images/defenderTop.bmp")
-			PS.imageBlit(image, defender.x  - 0, defender.y - 2);
-
-		if(image.source === "https://pjmanley671.github.io/game-files/SpaceInvaders/images/Shot.bmp")
-		{
-			for(i = 0; i < shots.length; i++)
-			{
-				PS.imageBlit(image, shots[i].x, shots[i].y);
+				gameStates.shot = true;
+				timerIntervals.defender = 0;
 			}
 		}
-
-		if(image.source === "https://pjmanley671.github.io/game-files/SpaceInvaders/images/UfoBase.bmp")
+		
+		if(!gameStates.ufo)
 		{
-			for(i = 0; i < ufos.length; i++)
+			timerIntervals.ufo++;
+			if(timerIntervals.ufo == 120)
 			{
-				PS.imageBlit(image, ufos[i].x-2, ufos[i].y+1);
+				gameStates.ufo = true;
+				timerIntervals.ufo = 0;
 			}
 		}
+		
+	};
+	
+	var OnTick_Draw = function()
+	{
+		
+	};
+	
+	var ClearList = function(iList)
+	{
+		var i, length;
+        length = iList.length;
 
-        if(image.source === "https://pjmanley671.github.io/game-files/SpaceInvaders/images/UfoMid.bmp")
-        {
-            for(i = 0; i < ufos.length; i++)
-            {
-                PS.imageBlit(image, ufos[i].x-1, ufos[i].y);
-            }
-        }
-
-        if(image.source === "https://pjmanley671.github.io/game-files/SpaceInvaders/images/UfoTop.bmp")
-        {
-        	for(i = 0; i < ufos.length; i++)
-        	{
-        		PS.imageBlit(image, ufos[i].x, ufos[i].y-1);
-			}
+		for (i = 0; length > i;)
+		{
+			iList[i] = null;
+			iList.shift();
+			length = iList.length;
 		}
 	};
-	var exports =
+	
+	var exports = 
+	{
+		Init : function()
 		{
-			init : function() // initializes the game space.
-			{
-				PS.gridSize(w, h);
-				PS.border(PS.ALL, PS.ALL, 0);
-
-				PS.statusText("UFO: Arrows + /, DEFCOM: WASD spacebar.");
-
-				winState = false;
-                ufoReady = true;
-                shotReady = true;
-
-				defender.setSpawn();
-				makeUfo();
-
-				sTimer = PS.timerStart(2, exports.shiftShots);
-				uTimer = PS.timerStart(160, exports.uAbility);
-				dTimer = PS.timerStart(4, exports.dAbility);
-				gTimer = PS.timerStart(1, exports.gameState);
-
-				exports.drawMap();
-			},
-
-			drawMap : function() // draws the current instance of the game.
-			{
-				PS.imageLoad("https://pjmanley671.github.io/game-files/SpaceInvaders/images/BackGround.bmp", myLoader, 1);
-				
-				PS.imageLoad("https://pjmanley671.github.io/game-files/SpaceInvaders/images/defenderBase.bmp", myLoader, 2);
-				PS.imageLoad("https://pjmanley671.github.io/game-files/SpaceInvaders/images/defenderBotMid.bmp", myLoader, 2);
-				PS.imageLoad("https://pjmanley671.github.io/game-files/SpaceInvaders/images/defenderTopMid.bmp", myLoader, 2);
-				PS.imageLoad("https://pjmanley671.github.io/game-files/SpaceInvaders/images/defenderTop.bmp", myLoader, 2);
-
-				PS.imageLoad("https://pjmanley671.github.io/game-files/SpaceInvaders/images/Shot.bmp", myLoader, 2);
-
-				PS.imageLoad("https://pjmanley671.github.io/game-files/SpaceInvaders/images/UfoBase.bmp", myLoader, 2);
-				PS.imageLoad("https://pjmanley671.github.io/game-files/SpaceInvaders/images/UfoMid.bmp", myLoader, 2);
-				PS.imageLoad("https://pjmanley671.github.io/game-files/SpaceInvaders/images/UfoTop.bmp", myLoader, 2);
-
-			},
-
-			pInput : function(key) // brings in the player input then performs actions accordingly.
-			{
-				if(winState === false)
-				{
-                    switch (key) { // defender movement + actions.
-                        case 119:
-                            //PS.debug("KEY === 'W'\n");
-                            defender.move(0, 0);
-                            break;
-                        case 100:
-                            //PS.debug("KEY === 'D'\n");
-                            defender.move(1, 0);
-                            break;
-                        case 97:
-                            //PS.debug("KEY === 'A'\n");
-                            defender.move(-1, 0);
-                            break;
-                        case 115:
-                            //PS.debug("KEY === 'S'\n");
-                            defender.move(0, 0);
-                            break;
-                        case 32:
-                            //PS.debug("KEY === 'SPACE_BAR'\n");
-                            makeShot();
-                            break;
-                        default:
-                            break;
-                    }
-
-                    switch (key) { // ufo moments + actions.
-                        case PS.KEY_ARROW_UP:
-                            //PS.debug("KEY === 'UP ARROW'\n");
-                            exports.shiftUfos(0, -1); // moves the ufo up
-                            break;
-                        case PS.KEY_ARROW_DOWN:
-                            //PS.debug("KEY === 'DOWN ARROW'\n");
-                            exports.shiftUfos(0, 1); // moves the ufo down
-                            break;
-                        case PS.KEY_ARROW_RIGHT:
-                            //PS.debug("KEY === 'RIGHT ARROW'\n");
-                            exports.shiftUfos(1, 0); // moves the ufo right
-                            break;
-                        case PS.KEY_ARROW_LEFT:
-                            //PS.debug("KEY === 'LEFT ARROW'\n");
-                            exports.shiftUfos(-1, 0); // moves the ufo left
-                            break;
-                        case 47:
-                            //PS.debug("KEY === 'forward slash'\n")
-                            makeUfo(); // creates a copy of the ufo.
-                            break;
-                        default:
-                            break;
-                    }
-                }else
-				{
-                    switch (key)
-					{
-                        case 114:
-                            exports.restart(); // restarts the game.
-                            break;
-                    }
-                }
-
-                exports.drawMap();
-			},
-
-			shiftUfos : function(cx, cy) // shifts the position of the ufos according change in x and change in y.
-			{
-				var i, cLoc;
-				for(i = 0; i < ufos.length; i++)
-				{
-					ufos[i].move(cx, cy);
-					cLoc = exports.checkCollision(ufos[i], shots);
-					if(cLoc !== -1)
-					{
-						exports.destroy(shots, cLoc);
-						exports.destroy(ufos, i);
-					}
-				}
-			},
-
-			shiftShots : function () // shifts the list of shots according to the sTimer.
-			{
-				var i, cLoc;
-				for(i = 0; i < shots.length; i++)
-				{
-					shots[i].move(0, -1);
-					if(exports.inBounds(shots[i].x, shots[i].y))
-					{
-                        cLoc = exports.checkCollision(shots[i], ufos);
-                        if (cLoc !== -1)
-                        {
-                        	exports.destroy(ufos, cLoc);
-                        	exports.destroy(shots, i);
-                        	PS.audioPlay("fx_coin7");
-                        }
-                    }else
-                    	{
-                    		exports.destroy(shots, i);
-                    	}
-				}
-				exports.drawMap();
-            },
-
-            inSpawn : function() // determines if a ufo from the list of ufos is in the spawn.
-			{
-				var i;
-
-				for(i = 0; i < ufos.length; i++)
-				{
-					if(ufos[i].x === Math.floor(w / 2) && ufos[i].y === 2)
-						return true;
-				}
-
-				return false;
-			},
-
-			inBounds : function(x, y) // determines if the passed in x and y position is in the game space.
-			{
-				if(x < 0 || y < 0)
-					return false;
-
-				if(x > w - 1 || y > h - 1)
-					return false;
-
-				return true;
-			},
-
-			checkCollision : function(obj, iList) // checks to see if a specific object collides with another object from a list.
-			{
-				var i;
-				for(i = 0; i < iList.length; i++)
-				{
-					if(iList[i].x === obj.x && iList[i].y === obj.y)
-						return i;
-				}
-				return -1;
-			},
-
-			ufoCollision : function(x, y) // checks to see if a ufo would collide with another ufo.
-			{
-				var i;
-
-				for(i = 0; i < ufos.length; i++)
-				{
-					if(ufos[i].x === x && ufos[i].y === y)
-						return true;
-				}
-			},
-
-			destroy : function(iList, i) // deletes a specific object from a list.
-			{
-				if(i !== iList.length - 1)
-				{
-					var tmp = iList[i];
-					iList[i] = iList[iList.length-1];
-					iList[iList.length-1] = tmp;
-				}
-
-				iList.pop();
-
-				if(ufos.length === 0)
-					winState = true;
-			},
-
-            uAbility : function() // resets the ufo ability to spawn in more clones.
-            {
-                ufoReady = true;
-            },
-
-			dAbility : function() // resets the defenders ability to create shots.
-			{
-				shotReady = true;
-			},
-
-			gameState : function() // responds to the state of the game such as if the game is finished.
-			{
-				if(winState === true)
-				{
-                    PS.timerStop(uTimer);
-                    PS.timerStop(sTimer);
-                    PS.timerStop(dTimer);
-                    PS.timerStop(gTimer);
-
-					if(ufos.length < 1)
-						PS.statusText("Defenders Win!");
-					else
-						PS.statusText("Invaders Win!");
-				}
-			},
-
-			clearList : function(iList) // clears a list of objects.
-			{
-                var i, length;
-
-                length = iList.length;
-
-                if(length > 0)
-                {
-                    for (i = 0; length > i;)
-                    {
-                        iList[i] = null;
-                        iList.shift();
-                        length = iList.length;
-                    }
-                }
-			},
-
-			close : function() // runs when the browser is closed.
-			{
-                PS.timerStop(uTimer);
-                PS.timerStop(sTimer);
-                PS.timerStop(dTimer);
-                PS.timerStop(gTimer);
-			},
-
-			restart : function() // restarts the game.
-			{
-				exports.clearList(shots);
-				exports.clearList(ufos);
-
-				exports.init();
-			}
-		};
-
+			PS.gridSize(width, height);
+			PS.border(PS.ALL, PS.ALL, 0);
+			PS.statusText("Space Invaders");
+			
+			gameStates.win = false;
+			gameStates.shot = true;
+			gameStates.ufo = true;
+			
+			ClearList(shots);
+			ClearList(ufos);
+			
+			myTimer = PS.timerStart(timerIntervals.update, OnTick);
+		},
+		
+		KeyPress : function(key, shift, ctrl, options)
+		{
+			
+		},
+		
+		Close : function()
+		{
+			PS.timerStop(OnTick);
+		}
+	};
+	
 	return exports;
 }();
 
-PS.init = G.init;
+PS.init = G.Init;
+PS.keyDown = G.KeyPress(key, shift, ctrl, options);
+PS.shutdown = G.Close();
 
 PS.touch = function( x, y, data, options ) {};
 
@@ -480,8 +162,6 @@ PS.enter = function( x, y, data, options ) {};
 PS.exit = function( x, y, data, options ) {};
 
 PS.exitGrid = function( options ) {};
-
-PS.keyDown = function( key, shift, ctrl, options ) {G.pInput(key)};
 
 PS.keyUp = function( key, shift, ctrl, options ) {};
 
@@ -495,5 +175,3 @@ PS.input = function( sensors, options ) {/*
 	 */};
 
 PS.swipe = function( data, options ) {};
-
-PS.shutdown = function( options ) {G.close;};
